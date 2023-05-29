b168d00f4aa859199c6d34641776027e3517b241
lucasseccatto <lucasseccatto1@gmail.com>
2023-05-29 13:58:37 -0300

    Games Club

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const abrir = () => {
+    document.querySelector('form').style.opacity = 1
+    document.querySelector('div').style.opacity = 0.3
+}
+
+const fechar = () => {
+    document.querySelector('form').style.opacity = 0
+    document.querySelector('div').style.opacity = 1
+}
